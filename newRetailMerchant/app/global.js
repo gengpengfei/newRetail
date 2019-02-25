96c26b963bffa1077dd93081d43624b8cf299f8d
gengpengfei <945908943@qq.com>
2019-02-25 11:58:34 +0800

    update global

--- newRetailMerchant/app/global.js
+++ newRetailMerchant/app/global.js
@@ -5,9 +5,9 @@ const {width, height} = Dimensions.get ('window');
 global.APP_DEBUG = __DEV__;
 
 if (__DEV__) {
-  global.global_BASEURL = 'http://139.224.220.33:8099'; //测试ip
+  global.global_BASEURL = ''; //测试ip
 } else {
-  global.global_BASEURL = 'http://139.224.220.33:8088'; //正式ip
+  global.global_BASEURL = ''; //正式ip
 }
 
 global.isIOS = Platform.OS === 'ios';
@@ -29,9 +29,7 @@ function isIphoneX () {
   }
 }
 
-global.appId =
-  'app_id=2088221309930993&method=alipay.trade.app.pay&format=JSON&charset=utf-8&sign_type=RSA2&version=1.0&return_url=&notify_url=http%3A%2F%2F139.224.220.33%3A8088%2FApi%2Fpay%2FalipayOrderNotify&timestamp=2018-04-28+11%3A15%3A32&sign=&biz_content=%7B%22out_trade_no%22%3A%222018042811153286595%22%2C%22body%22%3A%22subject-%5Cu6d4b%5Cu8bd5%22%2C%22total_fee%22%3A%220.01%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%7D';
-
+global.appId = ''
 global.statusbarHeight = Platform.OS === 'ios' ? (isIphoneX () ? 44 : 20) : 0; //状态栏的高度
 global.appBar_Height = Platform.OS === 'ios' ? 44 : 56;
 global.header_Height = Platform.select ({
